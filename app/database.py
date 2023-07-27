ae41d78206ada5d9b24b5cba906bec43aaa55c4a
nietsuu <nietsuu@gmail.com>
2023-07-27 14:34:59 +0800

    Implement DatabaseCache

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Dict
 from redis.asyncio import Redis
 from app.models import Task
 from app.environ import REDISHOST, REDISPORT, REDISUSER, REDISPASSWORD
@@ -7,18 +7,19 @@ from app.environ import REDISHOST, REDISPORT, REDISUSER, REDISPASSWORD
 
 class Database:
     def __init__(self) -> None:
-        self._conn: "Redis[Any]" = Redis(
+        self._connection: "Redis[Any]" = Redis(
             host=REDISHOST,
             port=REDISPORT,
             username=REDISUSER,
             password=REDISPASSWORD,
         )
+        self._cache = DatabaseCache(self)
 
     async def get_user_tasks(
         self, uid: str, *, category: Optional[str] = None
     ) -> List[Task]:
         tasks = []
-        tasks_data = await self._conn.lrange(f"tasks.{uid}", 0, -1)
+        tasks_data = await self._cache.lrange(f"tasks.{uid}")
 
         for raw_data in tasks_data:
             task = Task(**json.loads(raw_data))
@@ -39,7 +40,36 @@ class Database:
         return None
 
     async def add_user_task(self, uid: str, task: Task) -> None:
-        await self._conn.lpush(f"tasks.{uid}", task.as_json_str())
+        await self._cache.lpush(f"tasks.{uid}", task.as_json_str())
 
     async def remove_user_task(self, uid: str, task: Task) -> None:
-        await self._conn.lrem(f"tasks.{uid}", 1, task.as_json_str())
+        await self._cache.lrem(f"tasks.{uid}", task.as_json_str())
+
+
+class DatabaseCache:
+    def __init__(self, database: Database) -> None:
+        self.db = database
+        self._cache: Dict[str, Any] = {}
+
+    async def lrange(self, key: str, *, from_cache: bool = True) -> List[str]:
+        if from_cache:
+            cached = self._cache.get(key)
+
+            if cached is not None:
+                return cached
+
+        data = [d.decode() for d in await self.db._connection.lrange(key, 0, -1)]
+        self._cache[key] = data
+        return data
+
+    async def lpush(self, key: str, value: Any) -> None:
+        await self.db._connection.lpush(key, value)
+
+        try:
+            self._cache[key].append(value)
+        except KeyError:
+            await self.lrange(key, from_cache=False)
+
+    async def lrem(self, key: str, value: Any) -> None:
+        await self.db._connection.lrem(key, 1, value)
+        self._cache[key].remove(value)
