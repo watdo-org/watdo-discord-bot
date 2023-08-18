a746a6a3b03dc0a355abf51d021938d10629c2fd
nietsuu <nietsuu@gmail.com>
2023-08-18 16:25:35 +0800

    Implement Database

--- watdo/__init__.py
+++ watdo/__init__.py
@@ -1,11 +1,13 @@
 import asyncio
 from watdo.discord import Bot
+from watdo.database import Database
 from watdo.environ import DISCORD_TOKEN
 from watdo._main_runner import async_main_runner
 
 
 async def async_main(loop: asyncio.AbstractEventLoop) -> int:
-    bot = Bot(loop=loop)
+    db = Database()
+    bot = Bot(loop=loop, database=db)
 
     await bot.start(DISCORD_TOKEN)
 
--- watdo/discord/__init__.py
+++ watdo/discord/__init__.py
@@ -1,8 +1,10 @@
 import asyncio
 import discord
 from discord.ext import commands as dc
+from watdo.database import Database
 
 
 class Bot(dc.Bot):
-    def __init__(self, *, loop: asyncio.AbstractEventLoop) -> None:
+    def __init__(self, *, loop: asyncio.AbstractEventLoop, database: Database) -> None:
         super().__init__(loop=loop, command_prefix="$", intents=discord.Intents.all())
+        self.db = database
