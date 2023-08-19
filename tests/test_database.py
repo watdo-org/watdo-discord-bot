2e17b0eacd841cb741e22c71b45222aa30f2f3d9
nietsuu <nietsuu@gmail.com>
2023-08-19 23:55:45 +0800

    Add test for Database.lrange

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,27 +1,37 @@
 import asyncio
-from typing import cast, List
-import pytest
+from typing import cast, List, Any
 from watdo.database import Database
 from watdo.logging import debug_wall_time
 
-
-@pytest.fixture
-def loop() -> asyncio.AbstractEventLoop:
-    return asyncio.new_event_loop()
+loop = asyncio.new_event_loop()
 
 
 class TestDatabase:
-    def test_get(self, loop: asyncio.AbstractEventLoop) -> None:
+    def ensure_second_db_call_is_faster(
+        self, method: str, *args: Any, **kwargs: Any
+    ) -> None:
         db = Database()
         deltas: List[float] = []
 
         for _ in range(2):
+            func = getattr(db, method)
+            coro = func(*args, **kwargs)
+
             with debug_wall_time("") as result:
-                loop.run_until_complete(db.get("test"))
+                loop.run_until_complete(coro)
 
             deltas.append(cast(float, result["delta"]))
 
-        first_request_speed = deltas[0]
-        second_request_speed = deltas[1]
-        partida = first_request_speed / 4
-        assert partida > second_request_speed
+        assert deltas[0] > deltas[1]
+
+    def test_get(self) -> None:
+        self.ensure_second_db_call_is_faster(
+            "get",
+            "profile.6e0a3e69c27f45a1ae7596debcd01587",
+        )
+
+    def test_lrange(self) -> None:
+        self.ensure_second_db_call_is_faster(
+            "lrange",
+            "tasks:profile.6e0a3e69c27f45a1ae7596debcd01587",
+        )
